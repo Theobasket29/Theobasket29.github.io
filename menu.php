dca0037ce9a2e0144187956116197f7316b232da
Theobasket29 <blancotheo29@gmail.com>
2024-01-24 01:22:07 +0100

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="index.css">
+        <title>Ongle Stella</title>
+</head>
+
+<body>
+    <h2>Menu</h2>
+    <?php require "header.html";?>
+</body>
+</html>
