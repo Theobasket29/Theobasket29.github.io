de26c438dd7de69c9a9d14a32122bf8b702c335c
Theobasket29 <blancotheo29@gmail.com>
2024-05-25 02:35:14 +0200

    Create faux-plafond.php

---
+++ faux-plafond.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title>Faux-Plafond Stephane Blanco</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <?php include("header.html"); ?>
+    </body>
+</html>
