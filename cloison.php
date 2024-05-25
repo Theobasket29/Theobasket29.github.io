acd97487a0075d4afdb515ae7055fbdba032bde6
Theobasket29 <blancotheo29@gmail.com>
2024-05-25 02:35:19 +0200

    Create cloison.php

---
+++ cloison.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title>Cloison Stephane Blanco</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <?php include("header.html"); ?>
+    </body>
+</html>
