1ae969036c88dbe1cfb6cdefb03425a9378c81a5
Theobasket29 <blancotheo29@gmail.com>
2024-05-31 18:30:23 +0200

    create grille

---
+++ grille-placo.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title>Placo Stephane Blanco</title>
+        <link rel="stylesheet" href="index.css">
+        
+    </head>
+    <body>
+    <?php include("header.html"); ?>
+        <div class="grid-container">
+            <div class="grid-item"><img src="./Image/Cloison/Chantier Cloison Modulaire 1.jpeg" alt="Chantier Cloison 1"></div>
+            <div class="grid-item"><img src="./Image/Cloison/Chantier Cloison Modulaire 2.jpeg" alt="Chantier Cloison 2"></div>
+            <div class="grid-item"><img src="./Image/Cloison/Chantier Cloison Modulaire 3.jpeg" alt="Chantier Cloison 3"></div>
+            <div class="grid-item"><img src="./Image/Cloison/Chantier Cloison Modulaire 4.jpeg" alt="Chantier Cloison 4"></div>
+            <div class="grid-item"><img src="./Image/Cloison/Chantier Cloison Modulaire 5.jpeg" alt="Chantier Cloison 5"></div>
+            <div class="grid-item"><img src="./Image/Cloison/Chantier Cloison Modulaire 6.jpeg" alt="Chantier Cloison 6"></div>
+            <div class="grid-item"><img src="./Image/Cloison/Chantier Cloison Modulaire 7.jpeg" alt="Chantier Cloison 7"></div>
+            <div class="grid-item"><img src="./Image/Cloison/Chantier Cloison Modulaire 8.jpeg" alt="Chantier Cloison 8"></div>
+            <div class="grid-item"><img src="./Image/Cloison/Chantier Cloison Modulaire 9.jpeg" alt="Chantier Cloison 9"></div>
+        </div>
+    </body>
+</html>
