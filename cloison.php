49dcf2606a96698a40e983052370055d6a732bdc
Theobasket29 <blancotheo29@gmail.com>
2024-05-31 18:30:40 +0200

    update solution

--- cloison.php
+++ cloison.php
@@ -3,9 +3,10 @@
     <head>
         <meta charset="UTF-8">
         <title>Cloison Stephane Blanco</title>
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="index.css">
     </head>
     <body>
         <?php include("header.html"); ?>
+        <a href="./grille-cloison.php">D'autre Réalisation</a>
     </body>
 </html>
--- faux-plafond.php
+++ faux-plafond.php
@@ -3,9 +3,10 @@
     <head>
         <meta charset="UTF-8">
         <title>Faux-Plafond Stephane Blanco</title>
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="index.css">
     </head>
     <body>
         <?php include("header.html"); ?>
+        <a href="./grille-fp.php">D'autre Réalisation</a>
     </body>
 </html>
